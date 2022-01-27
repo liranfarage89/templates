f7d84eecede678c97bf50c4762c98e761313f171
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-01-27 15:21:39 +0200

    Update cluster.tf

--- aws/k8s/cluster.tf
+++ aws/k8s/cluster.tf
@@ -1,6 +1,6 @@
 module "my-cluster" {
-  #source = "github.com/env0/k8s-modules//aws?ref=chore-drop-oidc-provider-data"
-  source = "github.com/env0/k8s-modules//aws"
+  source = "github.com/env0/k8s-modules//aws?ref=chore-drop-oidc-provider-data"
+  #source = "github.com/env0/k8s-modules//aws"
 
   region       = var.region
   cluster_name = var.cluster_name
